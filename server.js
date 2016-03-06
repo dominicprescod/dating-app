432536701bc920b19b8154471acdf61c0f874a8c
Dominic Prescod <dominicprescod@gmail.com>
2016-03-06 12:42:15 -0500

    starter files/folders

---
+++ server.js
@@ -0,0 +1,31 @@
+// REQUIREMENTS
+var express           = require('express'),
+    bodyParser        = require('body-parser'),
+    methodOverride    = require('method-override'),
+    mongoose          = require('mongoose'),
+    passport          = require('passport'),
+    session           = require('express-session'),
+    app               = express(),
+    port              = process.env.PORT || 3000;
+
+
+// require('./config/passport')(passport);
+
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/dating_app';
+mongoose.connect(mongoUri);
+
+// MIDDELWARE
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ name: 'dating_app', secret: 'dating' }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.use(methodOverride('_method'));
+
+// CONNECT & LISTEN
+mongoose.connection.once('open', function() {
+  app.listen(port, function() {
+      console.log('LISTEN TO ME ON PORT ' + port);
+  });
+});
